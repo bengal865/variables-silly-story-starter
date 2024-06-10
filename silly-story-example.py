85a6397b4b23f8af4bb162304bd5548d424ecc03
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-10 11:24:35 -0400

    Add files via upload

---
+++ silly-story-example.py
@@ -0,0 +1,17 @@
+# Abraham Lincoln
+# 10 JUN 2024
+# Silly Story Example
+
+# Create and initialize our variables
+noun = 'car'
+adverb = 'stealthily'
+adjective = 'delicious'
+print(f'The {noun} was {adjective} as it {adverb} rolled into the parking space at the stadium.')
+
+noun2 = 'shopping cart'
+adverb2 = 'aggressively'
+adjective2 = 'exhausted'
+print(f'The {noun2} was {adjective2} before it {adverb2} skidded into the Coast Guard station in Traverse City.')
+
+
+
